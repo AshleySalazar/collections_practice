63193f5b86804d6ffd3e06d8cb2f4ac064018d94
Ashley Salazar <ashleysalazar0902@gmail.com>
2021-02-14 22:45:30 -0600

    commit tree e369c6eafb33b66ae82f3ff3f7ca6316627522d1

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,37 @@
+def sort_array_asc(array)
+    return array.sort
+end
 
+def sort_array_desc(array)
+    return array.sort{ |a, b| b <=> a }
+end
+
+def sort_array_char_count(array)
+    return array.sort{ |a, b| a.length <=> b.length }
+end
+
+def swap_elements(array)
+end
+
+def reverse_array(array)
+    return array.reverse
+end
+
+def kesha_maker(array)
+    arr = []
+    array.each do |item|
+        item[2] = ''
+        arr << item.insert(2, "$")
+    end
+
+    return arr
+end
+
+def find_a(array)
+end
+
+def sum_array(arra)
+end
+
+def add_s(array)
+end
